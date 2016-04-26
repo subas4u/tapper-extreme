f9880ac4e84358993b7534a515fcf3fa4ff58ee4
Subas Tharmabalan <subas4u@gmail.com>
2016-04-25 22:29:25 -0400

    Initial commit

--- tapper-extreme/ViewController.swift
+++ tapper-extreme/ViewController.swift
@@ -8,18 +8,99 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    //Properties
+    var maxTaps = 0;
+    var currentTaps = 0;
+
+    //Outlets
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var howManyTapsTxt: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    @IBOutlet weak var tapBtn: UIButton!
+    @IBOutlet weak var tapsLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.howManyTapsTxt.delegate = self;
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        animateViewMoving(true, moveValue: 200)
     }
-
-
+    func textFieldDidEndEditing(textField: UITextField) {
+        animateViewMoving(false, moveValue: 200)
+    }
+    
+    func animateViewMoving (up:Bool, moveValue :CGFloat){
+        let movementDuration:NSTimeInterval = 0.3
+        let movement:CGFloat = ( up ? -moveValue : moveValue)
+        UIView.beginAnimations( "animateView", context: nil)
+        UIView.setAnimationBeginsFromCurrentState(true)
+        UIView.setAnimationDuration(movementDuration )
+        self.view.frame = CGRectOffset(self.view.frame, 0,  movement)
+        UIView.commitAnimations()
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        howManyTapsTxt.resignFirstResponder()
+        return true
+    }
+    
+    @IBAction func onCoinTapped(sender: UIButton!) {
+        currentTaps += 1
+        updateTapsLbl()
+        
+        if isGameOver() {
+            restartGame()
+        }
+    }
+    
+    
+    @IBAction func onPlayBtnPressed(sender: UIButton) {
+        
+        if howManyTapsTxt.text != nil && howManyTapsTxt.text != "" {
+        
+            howManyTapsTxt.resignFirstResponder()
+            
+            playBtn.hidden = true
+            howManyTapsTxt.hidden = true
+            
+            tapBtn.hidden = false
+            tapsLbl.hidden = false
+            
+            maxTaps = Int (howManyTapsTxt.text!)!
+            currentTaps = 0
+            
+            updateTapsLbl()
+            
+        }
+    }
+    
+    func restartGame() {
+        maxTaps = 0
+        howManyTapsTxt.text = ""
+        
+        playBtn.hidden = false
+        howManyTapsTxt.hidden = false
+        
+        tapBtn.hidden = true
+        tapsLbl.hidden = true
+    }
+    
+    func isGameOver() -> Bool {
+        if currentTaps >= maxTaps {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func updateTapsLbl() {
+        tapsLbl.text = "\(currentTaps) Taps"
+    }
 }
 
